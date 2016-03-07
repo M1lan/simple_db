1e492ac6fd4d7b57029ebe72f4fc97bce458a841
Milan Santosi <milan.santosi@gmail.com>
2016-03-07 02:22:17 +0100

    that's it.

--- main.c
+++ main.c
@@ -9,29 +9,28 @@ int main(int argc, char *argv[])
   int value;
   char answer;
 
-  dbFile = fopen("simple_db.txt", "w");
+  dbFile = fopen("simple_db.txt", "a");
   if (dbFile == NULL)
     {
       printf("ERROR: Could not open simple_db.txt\n");
       exit(1);
     }
 
-  while(true)
-    {
-      printf("Key: ");
-      scanf("%s", key);
-      fprintf(dbFile, "%s ", key);
-      printf("Value: ");
-      scanf("%d", &value);
-      fprintf(dbFile, "%d\n", value);
+  while (answer != 'n')
+  {
+    printf("Key: ");
+    scanf("%s", key);
 
-      printf("Do you want to store another entry? (y/n) ");
-      answer = getchar();
-      if (answer == "n")
-        {
-          break;
-        }
-    }
+    fprintf(dbFile, "%s ", key);
+    printf("Value: ");
+
+    scanf("%d", &value);
+    fprintf(dbFile, "%d\n", value);
+
+    printf("Do you want to store another entry? (y/n): ");
+    scanf(" %c", &answer);
+  }
+
   fclose(dbFile);
   exit(0);
 }
